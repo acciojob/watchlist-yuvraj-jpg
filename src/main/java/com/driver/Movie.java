670366cd73c41cf5b143eb58c6b8513860a89373
Yuvraj <yv96875@gmail.com>
2023-02-21 21:56:08 +0530

    Watchlist

--- src/main/java/com/driver/Movie.java
+++ src/main/java/com/driver/Movie.java
@@ -1,11 +1,12 @@
 package com.driver;
 
 public class Movie {
-        private String name;
-        private int durationInMinutes;
-        private double imdbRating;
+    private String name;
+    private int durationInMinutes;
+    private double imdbRating;
 
     public Movie() {
+
     }
 
     public Movie(String name, int durationInMinutes, double imdbRating) {
